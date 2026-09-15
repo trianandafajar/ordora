c8cf912e589628c1c024994f43a1db68bfb3e1e7
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-09-15 23:44:58 +0700

    feat: add foreign key checks around truncation in ProductSeeder and TableSeeder for data integrity

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,12 +5,15 @@ namespace Database\Seeders;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
     public function run(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
         Product::query()->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
 
         $categories = Category::pluck('id', 'name');
 
--- database/seeders/TableSeeder.php
+++ database/seeders/TableSeeder.php
@@ -4,13 +4,16 @@ namespace Database\Seeders;
 
 use App\Models\Table;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class TableSeeder extends Seeder
 {
     public function run(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
         Table::query()->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
 
         for ($i = 1; $i <= 10; $i++) {
             Table::create([
