fdd38384e1e1369e56b22969bedaae22a0c9777c
Triananda Fajar <mail.trianandafajar@gmail.com>
2026-09-15 23:40:33 +0700

    feat: add foreign key checks around user truncation for data integrity

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,13 +5,16 @@ namespace Database\Seeders;
 use App\Enums\UserRole;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
         User::query()->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
 
         User::create([
             'name' => 'Admin',
